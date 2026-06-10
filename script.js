49c57bccfb90721f2d04a2e8e2b4819738fafc35
Lunna Moreno <lammm@cesar.school>
2026-06-10 16:50:33 -0300

    aba de contatos e rodapé

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 document.querySelectorAll(".menu-link").forEach(link => {
+
+    const href = link.getAttribute("href");
+    if (!href.startsWith("#")) return;
+
     link.addEventListener("click", function(e) {
         e.preventDefault();
-
-        const destino = document.querySelector(this.getAttribute("href"));
-
+        const destino = document.querySelector(href);
         const posicao =
             destino.offsetTop -
             (window.innerHeight / 2) +
             (destino.offsetHeight / 2);
-
         window.scrollTo({
             top: posicao,
             behavior: "smooth"
